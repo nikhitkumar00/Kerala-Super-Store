8af26621312486110fb48d1c5683693cc669871f
Nikhit Kumar <nikhitkumar00@gmail.com>
2024-02-04 17:09:27 +0530

    added text stroke

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,13 +1,9 @@
 const About = () => {
   return (
     <div className="content flex h-svh flex-col items-center justify-center gap-10 text-center sm:gap-44 md:flex-row">
-      <div
-        style={{ WebkitTextStroke: "3px black" }}
-        className="text-7xl font-extrabold text-transparent"
-      >
+      <div className="font-outline-2 text-7xl font-extrabold text-transparent">
         About
       </div>
-
       <div className="m-2 rounded-xl bg-white bg-opacity-30 p-6 text-lg font-semibold shadow-sm md:w-1/2">
         Kerala Super Store, situated at the heart of Aloor, stands as a
         prominent supermarket with a rich history. Originating as a grocery
